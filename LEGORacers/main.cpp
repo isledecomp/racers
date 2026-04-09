c267c03dd03bec13619f8c6f1d54c4f2a6735b58
Christian Semmler <mail@csemmler.com>
2026-04-08 21:41:57 -0700

    fix type

--- LEGORacers/main.cpp
+++ LEGORacers/main.cpp
@@ -44,7 +44,8 @@ void ParseCommandLine()
 	}
 
 	LegoS32 i;
-	for (; g_commandLineArgs.m_argc < sizeOfArray(g_commandLineArgs.m_argv); offset += i, g_commandLineArgs.m_argc++) {
+	for (; g_commandLineArgs.m_argc < (LegoS32) sizeOfArray(g_commandLineArgs.m_argv);
+		 offset += i, g_commandLineArgs.m_argc++) {
 		LegoChar* arg = &g_commandLine[offset];
 
 		for (i = 0; arg[i] && arg[i] != ' '; i++) {

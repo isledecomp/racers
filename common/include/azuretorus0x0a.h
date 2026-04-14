a9b7c17cbef0c4fc0d19d861db74603de018b400
disinvite <disinvite@users.noreply.github.com>
2026-04-13 21:50:22 -0400

    Build out more functions

--- common/include/azuretorus0x0a.h
+++ common/include/azuretorus0x0a.h
@@ -10,13 +10,20 @@ public:
 	AzureTorus0x0a();
 	~AzureTorus0x0a();
 
+	static LegoS32 FUN_00449dc0(undefined2* p_string);
 	undefined2* FUN_00449e50(undefined4 p_param);
-	undefined4 FUN_00449e70(undefined2* p_1, LegoS32 p_2);
+	undefined4 FUN_00449e70(undefined2* p_buf, LegoS32 p_count);
 	undefined4 FUN_00449ed0(AzureTorus0x0a* p_torus);
 	void FUN_00449f00();
 	void FUN_00449f30();
 	void FUN_00449f70();
 	undefined4 FUN_00449fc0(AzureTorus0x0a* p_torus);
+	undefined4 FUN_0044a020(AzureTorus0x0a* p_torus);
+	undefined4 FUN_0044a080(undefined2* p_string);
+	void ToUpperCase();
+	void FUN_0044a130(char* p_string);
+	void FUN_0044a160(char* p_buf);
+	LegoS32 FUN_0044a1a0();
 
 	void Reset()
 	{
--- common/src/azuretorus0x0a.cpp
+++ common/src/azuretorus0x0a.cpp
@@ -2,6 +2,22 @@
 
 #include "types.h"
 
+#include <ctype.h>
+#include <memory.h>
+
+// FUNCTION: LEGORACERS 0x449dc0
+LegoS32 AzureTorus0x0a::FUN_00449dc0(undefined2* p_string)
+{
+	// real strlen?
+	LegoS32 len = 0;
+
+	while (*p_string++) {
+		len++;
+	}
+
+	return len;
+}
+
 // FUNCTION: GOLDP 0x1002f950
 // FUNCTION: LEGORACERS 0x449e10
 AzureTorus0x0a::AzureTorus0x0a()
@@ -25,21 +41,21 @@ undefined2* AzureTorus0x0a::FUN_00449e50(undefined4 p_param)
 
 // FUNCTION: GOLDP 0x1002f9b0
 // FUNCTION: LEGORACERS 0x449e70
-undefined4 AzureTorus0x0a::FUN_00449e70(undefined2* p_1, LegoS32 p_2)
+undefined4 AzureTorus0x0a::FUN_00449e70(undefined2* p_buf, LegoS32 p_count)
 {
 	m_unk0x08 = 0;
 	m_unk0x06 = 0;
 	m_unk0x04 = 0;
-	m_unk0x00 = p_1;
+	m_unk0x00 = p_buf;
 	FUN_00449f00();
 
-	if (p_2 == 0) {
+	if (p_count == 0) {
 		m_unk0x04 = EightMinusSix() + 1;
 		return 1;
 	}
 
-	if (p_2 < EightMinusSix()) {
-		m_unk0x04 = p_2;
+	if (p_count < EightMinusSix()) {
+		m_unk0x04 = p_count;
 		return 1;
 	}
 
@@ -113,11 +129,90 @@ undefined4 AzureTorus0x0a::FUN_00449fc0(AzureTorus0x0a* p_torus)
 		return 0;
 	}
 
-	while (--len) {
+	while (--len >= 0) {
 		if (m_unk0x00[len] != *p_torus->FUN_00449e50(len)) {
 			return 0;
 		}
 	}
 
 	return 1;
+}
+
+// FUNCTION: LEGORACERS 0x44a020
+undefined4 AzureTorus0x0a::FUN_0044a020(AzureTorus0x0a* p_torus)
+{
+	LegoS32 len = FUN_00449dc0(p_torus->m_unk0x00);
+	if (len >= m_unk0x04) {
+		return 0;
+	}
+
+	memcpy(m_unk0x00, p_torus->m_unk0x00, 2 * len);
+	m_unk0x06 = p_torus->m_unk0x06;
+	m_unk0x08 = p_torus->m_unk0x08;
+
+	return 1;
+}
+
+// FUNCTION: LEGORACERS 0x44a080
+undefined4 AzureTorus0x0a::FUN_0044a080(undefined2* p_string)
+{
+	LegoS32 len = FUN_00449dc0(p_string);
+	if (len >= m_unk0x04) {
+		return 0;
+	}
+
+	memcpy(m_unk0x00, p_string, 2 * len);
+	m_unk0x00[len] = 0;
+	m_unk0x08 = len;
+
+	return 1;
+}
+
+// FUNCTION: LEGORACERS 0x44a0d0
+void AzureTorus0x0a::ToUpperCase()
+{
+	LegoS32 i = EightMinusSix();
+	while (--i >= 0) {
+		LegoU16 c = m_unk0x00[i];
+		if ((c >= 0x80 && c <= 0xe0) || islower(c)) {
+			m_unk0x00[i] = c + ('A' - 'a');
+		}
+	}
+}
+
+// FUNCTION: LEGORACERS 0x44a130
+void AzureTorus0x0a::FUN_0044a130(char* p_string)
+{
+	LegoS32 i = EightMinusSix();
+	p_string[i] = '\0';
+
+	while (--i >= 0) {
+		p_string[i] = m_unk0x00[i];
+	}
+}
+
+// FUNCTION: LEGORACERS 0x44a160
+void AzureTorus0x0a::FUN_0044a160(char* p_buf)
+{
+	LegoS32 i = EightMinusSix();
+	memset(p_buf, 0, 8);
+	i = i > 8 ? 8 : i;
+
+	while (--i >= 0) {
+		p_buf[i] = m_unk0x00[i];
+	}
+}
+
+// FUNCTION: LEGORACERS 0x44a1a0
+LegoS32 AzureTorus0x0a::FUN_0044a1a0()
+{
+	LegoS32 i = FUN_00449dc0(m_unk0x00);
+	LegoS32 count = 1;
+	while (--i >= 0) {
+		if (m_unk0x00[i] == '\n') {
+			count++;
+		}
+	}
+
+	return count;
 }

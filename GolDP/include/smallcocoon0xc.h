b144c449c1e10b8612a58193e28f11294bcdb3a7
Christian Semmler <mail@csemmler.com>
2026-04-17 11:57:58 -0700

    clang

--- GolDP/include/smallcocoon0xc.h
+++ GolDP/include/smallcocoon0xc.h
@@ -9,13 +9,13 @@
 class SmallCocoon0xc {
 public:
 	struct Item {
-		LegoChar m_key[8];    // 0x00;
+		LegoChar m_key[8];    // 0x00
 		undefined4* m_unk0x8; // 0x08
 	};
 
 	SmallCocoon0xc();
 
-	virtual ~SmallCocoon0xc();              //  vtable+0x00
+	virtual ~SmallCocoon0xc();               // vtable+0x00
 	virtual void VTable0x04(LegoU32 p_size); // vtable+0x04
 	virtual void VTable0x08();               // vtable+0x08
 

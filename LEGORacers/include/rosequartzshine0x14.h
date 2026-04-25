d5082a96f661edb52c80239727d5f63bf1974d02
Christian Semmler <mail@csemmler.com>
2026-04-24 19:05:00 -0700

    Implement/match `RoseQuartzShine0x14`

--- LEGORacers/include/rosequartzshine0x14.h
+++ LEGORacers/include/rosequartzshine0x14.h
@@ -1,20 +1,55 @@
 #ifndef ROSEQUARTZSHINE0X14_H
 #define ROSEQUARTZSHINE0X14_H
 
+#include "ceruleansubject0x0c.h"
+#include "compat.h"
 #include "decomp.h"
+#include "types.h"
 
 // VTABLE: LEGORACERS 0x004b08c4
 // SIZE 0x14
-class RoseQuartzShine0x14 {
+class RoseQuartzShine0x14 : public CeruleanSubject0x0c {
 public:
+	// SIZE 0x38
+	class Struct0x38 {
+	public:
+		Struct0x38();
+		~Struct0x38();
+
+		void FUN_004364b0();
+		void FUN_00436650();
+
+	private:
+		// SIZE 0x08
+		class Struct0x08 {
+		public:
+			undefined4 m_unk0x00; // 0x00
+			undefined4 m_unk0x04; // 0x04
+		};
+
+		undefined4 m_unk0x00; // 0x00
+		Struct0x08 m_unk0x04; // 0x04
+		Struct0x08 m_unk0x0c; // 0x0c
+		Struct0x08 m_unk0x14; // 0x14
+		Struct0x08 m_unk0x1c; // 0x1c
+		undefined4 m_unk0x24; // 0x24
+		undefined4 m_unk0x28; // 0x28
+		undefined4 m_unk0x2c; // 0x2c
+		undefined4 m_unk0x30; // 0x30
+		undefined2 m_unk0x34; // 0x34
+		undefined2 m_unk0x36; // 0x36
+	};
+
 	RoseQuartzShine0x14();
-	virtual ~RoseQuartzShine0x14(); // vtable+0x00
+	~RoseQuartzShine0x14() override; // vtable+0x00
+	void VTable0x08() override;      // vtable+0x08
 
 	// SYNTHETIC: LEGORACERS 0x00436680
 	// RoseQuartzShine0x14::`scalar deleting destructor'
 
 private:
-	undefined m_unk0x04[0x14 - 0x04]; // 0x04
+	Struct0x38* m_unk0x0c; // 0x0c
+	LegoU32 m_unk0x10;     // 0x10
 };
 
 #endif // ROSEQUARTZSHINE0X14_H
--- LEGORacers/src/rosequartzshine0x14.cpp
+++ LEGORacers/src/rosequartzshine0x14.cpp
@@ -1,17 +1,63 @@
 #include "rosequartzshine0x14.h"
 
-DECOMP_SIZE_ASSERT(RoseQuartzShine0x14, 0x14)
+#include <memory.h>
 
-// STUB: LEGORACERS 0x00436660
-RoseQuartzShine0x14::RoseQuartzShine0x14()
+DECOMP_SIZE_ASSERT(RoseQuartzShine0x14, 0x14)
+DECOMP_SIZE_ASSERT(RoseQuartzShine0x14::Struct0x38, 0x38)
+
+// FUNCTION: LEGORACERS 0x00436490
+RoseQuartzShine0x14::Struct0x38::Struct0x38()
 {
-	// TODO
-	STUB(0x436660);
+	FUN_004364b0();
 }
 
-// STUB: LEGORACERS 0x004366a0
+// FUNCTION: LEGORACERS 0x004364a0
+RoseQuartzShine0x14::Struct0x38::~Struct0x38()
+{
+	FUN_00436650();
+}
+
+// FUNCTION: LEGORACERS 0x004364b0
+void RoseQuartzShine0x14::Struct0x38::FUN_004364b0()
+{
+	m_unk0x00 = 0;
+	m_unk0x24 = 0;
+	m_unk0x28 = 0;
+	m_unk0x2c = 0;
+	m_unk0x30 = 0;
+	m_unk0x34 = 0;
+	memset(&m_unk0x04, 0, sizeof(m_unk0x04));
+	memset(&m_unk0x0c, 0, sizeof(m_unk0x0c));
+	memset(&m_unk0x14, 0, sizeof(m_unk0x14));
+	memset(&m_unk0x1c, 0, sizeof(m_unk0x1c));
+}
+
+// FUNCTION: LEGORACERS 0x00436650
+void RoseQuartzShine0x14::Struct0x38::FUN_00436650()
+{
+	FUN_004364b0();
+}
+
+// FUNCTION: LEGORACERS 0x00436660
+RoseQuartzShine0x14::RoseQuartzShine0x14()
+{
+	m_unk0x0c = NULL;
+	m_unk0x10 = 0;
+}
+
+// FUNCTION: LEGORACERS 0x004366a0
 RoseQuartzShine0x14::~RoseQuartzShine0x14()
 {
-	// TODO
-	STUB(0x4366a0);
+	VTable0x08();
+}
+
+// FUNCTION: LEGORACERS 0x004368f0
+void RoseQuartzShine0x14::VTable0x08()
+{
+	if (m_unk0x0c) {
+		delete[] m_unk0x0c;
+		m_unk0x0c = NULL;
+	}
+
+	m_unk0x10 = 0;
 }

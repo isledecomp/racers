ef1d7593a4db6bf21673908c93a36ef64872377c
jonschz <jonschz@users.noreply.github.com>
2026-04-26 15:39:42 +0200

    Implement VTable

--- GolDP/src/bronzefalcon0xc8770.cpp
+++ GolDP/src/bronzefalcon0xc8770.cpp
@@ -5,91 +5,354 @@ DECOMP_SIZE_ASSERT(BronzeFalcon0xc8770, 0xc8770)
 // STUB: GOLDP 0x100078e0
 BronzeFalcon0xc8770::BronzeFalcon0xc8770()
 {
-	// TODO
 	STUB(0x100078e0);
 }
 
 // STUB: GOLDP 0x10007980
 BronzeFalcon0xc8770::~BronzeFalcon0xc8770()
 {
-	// TODO
 	STUB(0x10007980);
 }
 
 // STUB: GOLDP 0x10007d80
 void BronzeFalcon0xc8770::VTable0x10()
 {
-	// TODO
 	STUB(0x10007d80);
 }
 
 // STUB: GOLDP 0x10008680
 void BronzeFalcon0xc8770::VTable0x04()
 {
-	// TODO
 	STUB(0x10008680);
 }
 
 // STUB: GOLDP 0x10008740
 void BronzeFalcon0xc8770::VTable0x18()
 {
-	// TODO
 	STUB(0x10008740);
 }
 
 // STUB: GOLDP 0x100087b0
 void BronzeFalcon0xc8770::VTable0x20(undefined4)
 {
-	// TODO
 	STUB(0x100087b0);
+}
+
+// STUB: GOLDP 0x100087e0
+void BronzeFalcon0xc8770::VTable0x5c()
+{
+	STUB(0x100087e0);
+}
+
+// STUB: GOLDP 0x10008910
+void BronzeFalcon0xc8770::VTable0x94()
+{
+	STUB(0x10008910);
+}
+
+// STUB: GOLDP 0x10008a50
+void BronzeFalcon0xc8770::VTable0xac()
+{
+	STUB(0x10008a50);
+}
+
+// STUB: GOLDP 0x10008c30
+void BronzeFalcon0xc8770::VTable0xb0()
+{
+	STUB(0x10008c30);
+}
+
+// STUB: GOLDP 0x10008dd0
+void BronzeFalcon0xc8770::VTable0x8c()
+{
+	STUB(0x10008dd0);
+}
+
+// STUB: GOLDP 0x10008f70
+void BronzeFalcon0xc8770::VTable0xa8()
+{
+	STUB(0x10008f70);
+}
+
+// STUB: GOLDP 0x100090b0
+void BronzeFalcon0xc8770::VTable0x88()
+{
+	STUB(0x100090b0);
+}
+
+// STUB: GOLDP 0x10009240
+void BronzeFalcon0xc8770::VTable0x9c()
+{
+	STUB(0x10009240);
+}
+
+// STUB: GOLDP 0x10009270
+void BronzeFalcon0xc8770::VTable0x98()
+{
+	STUB(0x10009270);
+}
+
+// STUB: GOLDP 0x100092a0
+void BronzeFalcon0xc8770::VTable0x90()
+{
+	STUB(0x100092a0);
+}
+
+// STUB: GOLDP 0x10009420
+void BronzeFalcon0xc8770::VTable0x54()
+{
+	STUB(0x10009420);
+}
+
+// STUB: GOLDP 0x10009540
+void BronzeFalcon0xc8770::VTable0xf0()
+{
+	STUB(0x10009540);
 }
 
 // STUB: GOLDP 0x10009640
 void BronzeFalcon0xc8770::VTable0x1c(undefined4)
 {
-	// TODO
 	STUB(0x10009640);
+}
+
+// STUB: GOLDP 0x10009780
+void BronzeFalcon0xc8770::VTable0xd8()
+{
+	STUB(0x10009780);
+}
+
+// STUB: GOLDP 0x100097c0
+void BronzeFalcon0xc8770::VTable0xd4()
+{
+	STUB(0x100097c0);
+}
+
+// STUB: GOLDP 0x100097f0
+void BronzeFalcon0xc8770::VTable0xdc()
+{
+	STUB(0x100097f0);
+}
+
+// STUB: GOLDP 0x10009840
+void BronzeFalcon0xc8770::VTable0xe0()
+{
+	STUB(0x10009840);
+}
+
+// STUB: GOLDP 0x10009860
+void BronzeFalcon0xc8770::VTable0xe4()
+{
+	STUB(0x10009860);
+}
+
+// STUB: GOLDP 0x100098d0
+void BronzeFalcon0xc8770::VTable0xe8()
+{
+	STUB(0x100098d0);
 }
 
 // STUB: GOLDP 0x10009950
 void BronzeFalcon0xc8770::VTable0x14()
 {
-	// TODO
 	STUB(0x10009950);
+}
+
+// STUB: GOLDP 0x10009960
+void BronzeFalcon0xc8770::VTable0x7c()
+{
+	STUB(0x10009960);
+}
+
+// STUB: GOLDP 0x10009990
+void BronzeFalcon0xc8770::VTable0x78()
+{
+	STUB(0x10009990);
+}
+
+// STUB: GOLDP 0x100099b0
+void BronzeFalcon0xc8770::VTable0x74()
+{
+	STUB(0x100099b0);
+}
+
+// STUB: GOLDP 0x10009a20
+void BronzeFalcon0xc8770::VTable0x70()
+{
+	STUB(0x10009a20);
+}
+
+// STUB: GOLDP 0x10009a70
+void BronzeFalcon0xc8770::VTable0xb4()
+{
+	STUB(0x10009a70);
+}
+
+// STUB: GOLDP 0x10009b40
+void BronzeFalcon0xc8770::VTable0x68()
+{
+	STUB(0x10009b40);
+}
+
+// STUB: GOLDP 0x10009b70
+void BronzeFalcon0xc8770::VTable0x64()
+{
+	STUB(0x10009b70);
+}
+
+// STUB: GOLDP 0x10009ba0
+void BronzeFalcon0xc8770::VTable0x6c()
+{
+	STUB(0x10009ba0);
+}
+
+// STUB: GOLDP 0x10009bd0
+void BronzeFalcon0xc8770::VTable0x80()
+{
+	STUB(0x10009bd0);
+}
+
+// STUB: GOLDP 0x10009ce0
+void BronzeFalcon0xc8770::VTable0x84()
+{
+	STUB(0x10009ce0);
 }
 
 // STUB: GOLDP 0x1000a110
 void BronzeFalcon0xc8770::VTable0x0c(undefined4, undefined4, undefined4)
 {
-	// TODO
 	STUB(0x1000a110);
+}
+
+// STUB: GOLDP 0x1000a1c0
+void BronzeFalcon0xc8770::VTable0xf8()
+{
+	STUB(0x1000a1c0);
+}
+
+// STUB: GOLDP 0x1000a1f0
+void BronzeFalcon0xc8770::VTable0xfc()
+{
+	STUB(0x1000a1f0);
+}
+
+// STUB: GOLDP 0x1000a210
+void BronzeFalcon0xc8770::VTable0x100()
+{
+	STUB(0x1000a210);
+}
+
+// STUB: GOLDP 0x1000a240
+void BronzeFalcon0xc8770::VTable0x104()
+{
+	STUB(0x1000a240);
+}
+
+// STUB: GOLDP 0x1000a260
+void BronzeFalcon0xc8770::VTable0x108()
+{
+	STUB(0x1000a260);
+}
+
+// STUB: GOLDP 0x1000a290
+void BronzeFalcon0xc8770::VTable0x10c()
+{
+	STUB(0x1000a290);
+}
+
+// STUB: GOLDP 0x1000aeb0
+void BronzeFalcon0xc8770::VTable0xb8()
+{
+	STUB(0x1000aeb0);
+}
+
+// STUB: GOLDP 0x1000aef0
+void BronzeFalcon0xc8770::VTable0xbc()
+{
+	STUB(0x1000aef0);
+}
+
+// STUB: GOLDP 0x1000af20
+void BronzeFalcon0xc8770::VTable0xc0()
+{
+	STUB(0x1000af20);
+}
+
+// STUB: GOLDP 0x1000af70
+void BronzeFalcon0xc8770::VTable0xc4()
+{
+	STUB(0x1000af70);
 }
 
 // STUB: GOLDP 0x1000af90
 void BronzeFalcon0xc8770::VTable0x28()
 {
-	// TODO
 	STUB(0x1000af90);
 }
 
 // STUB: GOLDP 0x1000b000
 void BronzeFalcon0xc8770::VTable0x2c(undefined4)
 {
-	// TODO
 	STUB(0x1000b000);
 }
 
 // STUB: GOLDP 0x1000b0c0
 void BronzeFalcon0xc8770::VTable0x30(undefined4)
 {
-	// TODO
 	STUB(0x1000b0c0);
+}
+
+// STUB: GOLDP 0x1000b1f0
+void BronzeFalcon0xc8770::VTable0x60()
+{
+	STUB(0x1000b1f0);
+}
+
+// STUB: GOLDP 0x1000b240
+void BronzeFalcon0xc8770::VTable0xc8()
+{
+	STUB(0x1000b240);
+}
+
+// STUB: GOLDP 0x1000b260
+void BronzeFalcon0xc8770::VTable0xcc()
+{
+	STUB(0x1000b260);
+}
+
+// STUB: GOLDP 0x1000b280
+void BronzeFalcon0xc8770::VTable0x3c()
+{
+	STUB(0x1000b280);
+}
+
+// STUB: GOLDP 0x1000b2b0
+void BronzeFalcon0xc8770::VTable0x40()
+{
+	STUB(0x1000b2b0);
+}
+
+// STUB: GOLDP 0x1000b2c0
+void BronzeFalcon0xc8770::VTable0x4c()
+{
+	STUB(0x1000b2c0);
+}
+
+// STUB: GOLDP 0x1000b350
+void BronzeFalcon0xc8770::VTable0x50()
+{
+	STUB(0x1000b350);
+}
+
+// STUB: GOLDP 0x1000b3d0
+void BronzeFalcon0xc8770::VTable0x58()
+{
+	STUB(0x1000b3d0);
 }
 
 // STUB: GOLDP 0x10028a10
 void BronzeFalcon0xc8770::VTable0x00()
 {
-	// TODO
 	STUB(0x10028a10);
 }
 
@@ -105,21 +368,65 @@ void BronzeFalcon0xc8770::FUN_10028ae0(SmallCocoon0xc* p_param)
 	STUB(0x10028ae0);
 }
 
-// FUNCTION: GOLDP 0x10029920 FOLDED
-void BronzeFalcon0xc8770::VTable0x38()
+// STUB: GOLDP 0x10029500
+void BronzeFalcon0xc8770::VTable0xa4()
 {
+	STUB(0x10029500);
+}
+
+// STUB: GOLDP 0x10029680
+void BronzeFalcon0xc8770::VTable0xa0()
+{
+	STUB(0x10029680);
+}
+
+// STUB: GOLDP 0x10029930
+void BronzeFalcon0xc8770::VTable0x44()
+{
+	STUB(0x10029930);
+}
+
+// STUB: GOLDP 0x10029940
+void BronzeFalcon0xc8770::VTable0x48()
+{
+	STUB(0x10029940);
 }
 
 // STUB: GOLDP 0x10029970
 void BronzeFalcon0xc8770::VTable0x24()
 {
-	// TODO
 	STUB(0x10029970);
+}
+
+// FUNCTION: GOLDP 0x10029920 FOLDED
+void BronzeFalcon0xc8770::VTable0x38()
+{
+}
+
+// FUNCTION: GOLDP 0x10029920 FOLDED
+void BronzeFalcon0xc8770::VTable0xd0()
+{
 }
 
 // STUB: GOLDP 0x1002c010 FOLDED
 void BronzeFalcon0xc8770::VTable0x34(LegoS32, const LegoFloat*)
 {
-	// TODO
 	STUB(0x1002c010);
+}
+
+// FUNCTION: GOLDP 0x1002c020 FOLDED
+void BronzeFalcon0xc8770::VTable0xec(undefined4*)
+{
+}
+
+// FUNCTION: GOLDP 0x10029920 FOLDED
+void BronzeFalcon0xc8770::VTable0xf4()
+{
+}
+
+// FUNCTION: GOLDP 0x100016f0 FOLDED
+LegoBool32 BronzeFalcon0xc8770::VTable0x110() const
+{
+	STUB(0x100016f0);
+	return FALSE;
 }

b7abcebc095c697601619eaa7055cf0db6fc3f00
disinvite <disinvite@users.noreply.github.com>
2026-04-30 22:44:03 -0400

    Stubs for more Imaginary* classes

--- LEGORacers/src/chromespike0x4.cpp
+++ LEGORacers/src/chromespike0x4.cpp
@@ -1,5 +1,10 @@
 #include "chromespike0x4.h"
 
+#include "golerror.h"
+#include "imaginarydrill0x2450.h"
+#include "imaginaryhammer0x658.h"
+#include "imaginarywrench0x23bc.h"
+
 DECOMP_SIZE_ASSERT(ChromeSpike0x4, 0x4)
 
 // STUB: LEGORACERS 0x0047f4c0
@@ -15,6 +20,33 @@ LegoBool32 ChromeSpike0x4::FUN_0047f4d0()
 	// TODO
 	STUB(0x47f4d0);
 	return TRUE;
+}
+
+// FUNCTION: LEGORACERS 0x0047f4e0
+ImaginaryTool0x368* ChromeSpike0x4::FUN_0047f4e0(undefined4 p_param)
+{
+	ImaginaryTool0x368* tool;
+	switch (p_param) {
+	case 0x01:
+	case 0x27:
+	case 0x28:
+	case 0x29:
+		tool = new ImaginaryHammer0x658();
+		break;
+
+	case 0x02:
+		tool = new ImaginaryDrill0x2450();
+
+	case 0x10:
+		tool = new ImaginaryWrench0x23bc();
+		break;
+	}
+
+	if (!tool) {
+		GOL_FATALERROR(c_golErrorOutOfMemory);
+	}
+
+	return tool;
 }
 
 // STUB: LEGORACERS 0x0049eb10

edbe916a30392ef21c2430d60120529bace7b0c7
jonschz <jonschz@users.noreply.github.com>
2026-05-22 23:04:18 +0200

    Re-enable annotations

--- common/src/golconstants.cpp
+++ common/src/golconstants.cpp
@@ -1,11 +1,11 @@
 #include "golconstants.h"
 
-// // GLOBAL: LEGORACERS 0x004c706c
-// // GLOBAL: GOLDP 0x10065b54
+// GLOBAL: LEGORACERS 0x004c706c
+// GLOBAL: GOLDP 0x10065b54
 const FloatConstant g_floatConst4096(4096.0f);
 
-// // GLOBAL: LEGORACERS 0x004c7074
-// // GLOBAL: GOLDP 0x10065b5c
+// GLOBAL: LEGORACERS 0x004c7074
+// GLOBAL: GOLDP 0x10065b5c
 const FloatConstant g_floatConst32(32.0f);
 
 // GLOBAL: LEGORACERS 0x004b0e08

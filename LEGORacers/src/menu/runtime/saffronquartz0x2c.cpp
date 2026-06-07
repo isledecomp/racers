8f4c3b64174b331d07e7db01abd0b5fe6f8e0297
jonschz <jonschz@users.noreply.github.com>
2026-06-07 22:02:46 +0200

    Improve `FUN_00404a10`

--- LEGORacers/src/menu/runtime/saffronquartz0x2c.cpp
+++ LEGORacers/src/menu/runtime/saffronquartz0x2c.cpp
@@ -74,14 +74,13 @@ LegoU32 SaffronQuartz0x2c::Frame0xb8::Model0x68::Reset()
 	return result;
 }
 
-// STUB: LEGORACERS 0x00404a10
+// FUNCTION: LEGORACERS 0x00404a10
 LegoU32 SaffronQuartz0x2c::Frame0xb8::Model0x68::FUN_00404a10(SaffronQuartz0x2c* p_parent, GolFileParser* p_parser)
 {
-	STUB(0x00404a10);
-
-	LegoU32 duration = 0;
+	LegoU32 duration;
 	m_unk0x28 = p_parent;
 
+	duration = 0;
 	p_parser->AssertNextTokenIs(GolFileParser::e_unknown0x2e);
 	::strncpy(m_name, p_parser->ReadStringWithMaxLength(8), sizeof(m_name));
 	p_parser->ReadLeftCurly();
@@ -89,15 +88,6 @@ LegoU32 SaffronQuartz0x2c::Frame0xb8::Model0x68::FUN_00404a10(SaffronQuartz0x2c*
 	GolFileParser::ParserTokenType token = p_parser->GetNextToken();
 	while (token != GolFileParser::e_rightCurly) {
 		switch (token) {
-		case GolFileParser::e_unknown0x2b:
-			m_unk0x0c = p_parser->ReadInteger();
-			break;
-		case GolFileParser::e_unknown0x2c:
-			duration = p_parser->ReadInteger();
-			break;
-		case GolFileParser::e_unknown0x2d:
-			m_unk0x58 = p_parser->ReadInteger();
-			break;
 		case GolFileParser::e_unknown0x2f:
 			::strncpy(m_unk0x2c.m_name, p_parser->ReadStringWithMaxLength(8), sizeof(m_unk0x2c.m_name));
 			m_unk0x20 = 1;
@@ -115,19 +105,16 @@ LegoU32 SaffronQuartz0x2c::Frame0xb8::Model0x68::FUN_00404a10(SaffronQuartz0x2c*
 			m_unk0x2c.m_indexedRef.m_modelIndex = p_parser->ReadInteger();
 			m_unk0x20 = 4;
 			break;
-		case GolFileParser::e_unknown0x33:
-			m_unk0x34.m_x = p_parser->ReadFloat();
-			m_unk0x34.m_y = p_parser->ReadFloat();
-			m_unk0x34.m_z = p_parser->ReadFloat();
+		case GolFileParser::e_unknown0x2b:
+			m_unk0x0c = p_parser->ReadInteger();
 			break;
-		case GolFileParser::e_unknown0x34:
-			m_unk0x40.m_x = p_parser->ReadFloat();
-			m_unk0x40.m_y = p_parser->ReadFloat();
-			m_unk0x40.m_z = p_parser->ReadFloat();
-			m_unk0x4c.m_x = p_parser->ReadFloat();
-			m_unk0x4c.m_y = p_parser->ReadFloat();
-			m_unk0x4c.m_z = p_parser->ReadFloat();
+		case GolFileParser::e_unknown0x2c:
+			duration = p_parser->ReadInteger();
 			break;
+		case GolFileParser::e_unknown0x2d:
+			m_unk0x58 = p_parser->ReadInteger();
+			break;
+
 		case GolFileParser::e_unknown0x36: {
 			m_unk0x5c = p_parser->ReadBracketedCountAndLeftCurly();
 			if (!m_unk0x5c) {
@@ -150,6 +137,19 @@ LegoU32 SaffronQuartz0x2c::Frame0xb8::Model0x68::FUN_00404a10(SaffronQuartz0x2c*
 			p_parser->ReadRightCurly();
 			break;
 		}
+		case GolFileParser::e_unknown0x33:
+			m_unk0x34.m_x = p_parser->ReadFloat();
+			m_unk0x34.m_y = p_parser->ReadFloat();
+			m_unk0x34.m_z = p_parser->ReadFloat();
+			break;
+		case GolFileParser::e_unknown0x34:
+			m_unk0x40.m_x = p_parser->ReadFloat();
+			m_unk0x40.m_y = p_parser->ReadFloat();
+			m_unk0x40.m_z = p_parser->ReadFloat();
+			m_unk0x4c.m_x = p_parser->ReadFloat();
+			m_unk0x4c.m_y = p_parser->ReadFloat();
+			m_unk0x4c.m_z = p_parser->ReadFloat();
+			break;
 		default:
 			break;
 		}
@@ -157,8 +157,8 @@ LegoU32 SaffronQuartz0x2c::Frame0xb8::Model0x68::FUN_00404a10(SaffronQuartz0x2c*
 		token = p_parser->GetNextToken();
 	}
 
-	duration += m_unk0x0c;
-	m_unk0x10 = duration;
+	// duration += m_unk0x0c;
+	m_unk0x10 = m_unk0x0c + duration;
 	return duration;
 }
 

2ee8c34794b0e15d0f0501c00a3bbf6a349e7472
jonschz <jonschz@users.noreply.github.com>
2026-06-20 23:00:26 +0200

    More `SpanRasterizerCallback` stubs

--- GolDP/include/render/golrasterizers1.h
+++ GolDP/include/render/golrasterizers1.h
@@ -7,23 +7,157 @@ class GolSoftwareRenderer;
 
 void FUN_10032c80(GolSoftwareRenderer* p_renderer);
 void FUN_100330d0(GolSoftwareRenderer* p_renderer, MipmapLevel* p_mipmap);
+
+void FUN_10033270();
+void FUN_100332a0();
+void FUN_100332d0();
+void FUN_100334d0();
+void FUN_100336d0();
+void FUN_10033890();
+void FUN_10033900();
+void FUN_10033a20();
+void FUN_10033b40();
+void FUN_10033b40();
+void FUN_10033d90();
+void FUN_10033fe0();
+void FUN_10034240();
+void FUN_100344a0();
+void FUN_100345e0();
+void FUN_10034720();
+void FUN_10034850();
 void FUN_10034980();
 void FUN_10034b10();
+void FUN_10034c40();
 void FUN_10034db0();
+void FUN_10034ea0();
+void FUN_10034fc0();
+void FUN_100350e0();
+void FUN_100350e0();
+void FUN_10035330();
+void FUN_10035580();
+void FUN_100357d0();
+void FUN_10035a20();
+void FUN_10035ca0();
+void FUN_10035dd0();
+void FUN_10035b60();
 void FUN_10035f00();
 void FUN_10036090();
+void FUN_100361c0();
 void FUN_10036330();
+void FUN_10036420();
+void FUN_10036540();
+void FUN_10036660();
+void FUN_100368b0();
+void FUN_100368b0();
+void FUN_10036b00();
+void FUN_10036d50();
+void FUN_10036fa0();
+void FUN_100370e0();
+void FUN_10037220();
+void FUN_10037350();
 void FUN_10037480();
 void FUN_10037610();
+void FUN_10037740();
 void FUN_100378b0();
+void FUN_100379a0();
+void FUN_10037ac0();
+void FUN_10037be0();
+void FUN_10037e30();
+void FUN_10037e30();
+void FUN_10038080();
+void FUN_100382d0();
+void FUN_10038520();
+void FUN_10038660();
+void FUN_100387a0();
+void FUN_100388d0();
 void FUN_10038a00();
 void FUN_10038b90();
+void FUN_10038cc0();
 void FUN_10038e30();
+void FUN_10038f20();
+void FUN_10039040();
+void FUN_10039160();
+void FUN_100393b0();
+void FUN_100393b0();
+void FUN_10039600();
+void FUN_10039850();
+void FUN_10039aa0();
+void FUN_10039be0();
+void FUN_10039d20();
+void FUN_10039e50();
 void FUN_10039f80();
 void FUN_1003a110();
+void FUN_1003a240();
 void FUN_1003a3b0();
+void FUN_1003a4a0();
+void FUN_1003a5c0();
+void FUN_1003a6e0();
+void FUN_1003a930();
+void FUN_1003a930();
+void FUN_1003ab80();
+void FUN_1003add0();
+void FUN_1003b020();
+void FUN_1003b160();
+void FUN_1003b2a0();
+void FUN_1003b3d0();
 void FUN_1003b500();
 void FUN_1003b690();
+void FUN_1003b7c0();
 void FUN_1003b930();
+
+void FUN_1003ba30(
+	GolSoftwareRenderer* p_renderer,
+	D3DTLVERTEX* p_vertex0,
+	D3DTLVERTEX* p_vertex1,
+	D3DTLVERTEX* p_vertex2
+);
+void FUN_1003c780(
+	GolSoftwareRenderer* p_renderer,
+	D3DTLVERTEX* p_vertex0,
+	D3DTLVERTEX* p_vertex1,
+	D3DTLVERTEX* p_vertex2
+);
+void FUN_1003cf40(
+	GolSoftwareRenderer* p_renderer,
+	D3DTLVERTEX* p_vertex0,
+	D3DTLVERTEX* p_vertex1,
+	D3DTLVERTEX* p_vertex2
+);
+void FUN_1003d700(
+	GolSoftwareRenderer* p_renderer,
+	D3DTLVERTEX* p_vertex0,
+	D3DTLVERTEX* p_vertex1,
+	D3DTLVERTEX* p_vertex2
+);
+void FUN_1003e590(
+	GolSoftwareRenderer* p_renderer,
+	D3DTLVERTEX* p_vertex0,
+	D3DTLVERTEX* p_vertex1,
+	D3DTLVERTEX* p_vertex2
+);
+void FUN_1003ee90(
+	GolSoftwareRenderer* p_renderer,
+	D3DTLVERTEX* p_vertex0,
+	D3DTLVERTEX* p_vertex1,
+	D3DTLVERTEX* p_vertex2
+);
+void FUN_1003f790(
+	GolSoftwareRenderer* p_renderer,
+	D3DTLVERTEX* p_vertex0,
+	D3DTLVERTEX* p_vertex1,
+	D3DTLVERTEX* p_vertex2
+);
+void FUN_1003fce0(
+	GolSoftwareRenderer* p_renderer,
+	D3DTLVERTEX* p_vertex0,
+	D3DTLVERTEX* p_vertex1,
+	D3DTLVERTEX* p_vertex2
+);
+void FUN_10040670(
+	GolSoftwareRenderer* p_renderer,
+	D3DTLVERTEX* p_vertex0,
+	D3DTLVERTEX* p_vertex1,
+	D3DTLVERTEX* p_vertex2
+);
 
 #endif // GOLRASTERIZERS1_H

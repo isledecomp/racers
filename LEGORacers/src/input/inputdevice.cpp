d235eb5829d4bf289f69da8a702cda737e4a88d6
Christian Semmler <mail@csemmler.com>
2026-07-03 13:40:40 -0700

    Merge interleaved compilation units into address-local files

--- LEGORacers/src/input/inputdevice.cpp
+++ LEGORacers/src/input/inputdevice.cpp
@@ -1,6 +1,9 @@
-#include "input/inputdevice.h"
-
 #include "golerror.h"
+#include "input/inputdevice.h"
+#include "input/inputmanager.h"
+#include "input/joystickdevice.h"
+#include "input/keyboarddevice.h"
+#include "input/mousedevice.h"
 
 #include <dinput.h>
 #include <string.h>
@@ -10,6 +13,8 @@ DECOMP_SIZE_ASSERT(InputDevice::DirectionalTrigger, 0x24)
 
 // GLOBAL: LEGORACERS 0x004c7080
 undefined2 g_defaultMapping[256];
+
+DECOMP_SIZE_ASSERT(InputManager, 0xf0)
 
 // FUNCTION: LEGORACERS 0x0044b8f0
 InputDevice::InputDevice()
@@ -367,6 +372,85 @@ undefined4 InputDevice::Unacquire()
 	return m_acquired;
 }
 
+// FUNCTION: LEGORACERS 0x0044bf20
+InputManager::InputManager()
+{
+	Reset();
+}
+
+// FUNCTION: LEGORACERS 0x0044bf60
+InputManager::~InputManager()
+{
+}
+
+// FUNCTION: LEGORACERS 0x0044bf70
+LegoS32 InputManager::Reset()
+{
+	LegoS32 result = 0;
+	m_initialized = FALSE;
+	m_joystickCount = 0;
+	m_mouseAvailable = FALSE;
+	m_keyboardAvailable = FALSE;
+	m_mouse = NULL;
+	m_keyboard = NULL;
+	memset(m_joysticks, 0, sizeof(m_joysticks));
+	memset(m_suspendedJoysticks, 0, sizeof(m_suspendedJoysticks));
+	memset(m_joystickPresent, 0, sizeof(m_joystickPresent));
+	m_mouseSuspended = FALSE;
+	m_keyboardSuspended = FALSE;
+	return result;
+}
+
+// FUNCTION: LEGORACERS 0x0044bfd0
+void InputManager::RestoreSuspendedDevices()
+{
+	for (LegoU32 i = 0; i < sizeOfArray(m_suspendedJoysticks); i++) {
+		if (m_suspendedJoysticks[i]) {
+			m_suspendedJoysticks[i] = !m_joysticks[i]->Acquire();
+		}
+	}
+
+	if (m_keyboardSuspended && m_keyboard) {
+		m_keyboardSuspended = !m_keyboard->Acquire();
+	}
+
+	if (m_mouseSuspended && m_mouse) {
+		m_mouseSuspended = !m_mouse->Acquire();
+	}
+}
+
+// FUNCTION: LEGORACERS 0x0044c040
+void InputManager::SuspendActiveDevices()
+{
+	for (LegoU32 i = 0; i < sizeOfArray(m_joysticks); i++) {
+		if (m_joysticks[i] && m_joysticks[i]->IsAcquired()) {
+			m_suspendedJoysticks[i] = TRUE;
+			m_joysticks[i]->Unacquire();
+		}
+	}
+
+	if (m_keyboard && m_keyboard->IsAcquired()) {
+		m_keyboardSuspended = TRUE;
+		m_keyboard->Unacquire();
+	}
+
+	if (m_mouse && m_mouse->IsAcquired()) {
+		m_mouseSuspended = TRUE;
+		m_mouse->Unacquire();
+	}
+}
+
+// FUNCTION: LEGORACERS 0x0044c0c0
+JoystickDevice* InputManager::FindJoystickByDeviceId(LegoS32 p_deviceId) const
+{
+	for (LegoS32 i = 0; i < m_joystickCount; i++) {
+		if (m_joysticks[i]->GetDeviceId() == p_deviceId) {
+			return m_joysticks[i];
+		}
+	}
+
+	return NULL;
+}
 // FUNCTION: LEGORACERS 0x0044c100
 InputDevice::DirectionalTrigger::DirectionalTrigger()
 {
